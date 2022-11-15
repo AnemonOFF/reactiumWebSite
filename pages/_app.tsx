c3bcca24e857f43942c0a1812d44f2b417a8a69f
AnemonOFF <43379450+AnemonOFF@users.noreply.github.com>
2022-11-16 03:26:06 +0500

    Icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import type { AppProps } from 'next/app';
-import { createTheme, darkTheme, lightTheme, ReactiumTheme, ReactiumThemeProvider } from '../reactiumui';
+import { darkTheme, lightTheme, ReactiumThemeProvider } from '../reactiumui';
 import '../globalStyles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const theme: ReactiumTheme = {
-    name: 'testtheme',
-    theme: createTheme({
-      colors: {
-        text: 'red',
-        primary: 'green'
-      }
-    })
-  }
-
   return (
       <ReactiumThemeProvider customThemes={[lightTheme, darkTheme]} defaultTheme='light'>
         <Component {...pageProps} />
